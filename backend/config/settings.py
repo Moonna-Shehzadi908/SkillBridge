b1bc4af378ba1a052f29fab5eae9f13c377abf68
Moonna-Shehzadi908 <218383071+Moonna-Shehzadi908@users.noreply.github.com>
2026-09-19 22:41:04 +0500

    Update backend settings for Vercel deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
-# Application definition
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
