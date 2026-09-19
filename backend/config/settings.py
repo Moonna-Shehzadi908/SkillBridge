ca817b7fbdccb44ad5ecd46a0e937baae7a7e174
Moonna-Shehzadi908 <218383071+Moonna-Shehzadi908@users.noreply.github.com>
2026-09-19 16:50:23 +0500

    Configure Render allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 # Application definition
 
 INSTALLED_APPS = [
